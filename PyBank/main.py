fdf80b50743137ecf950508af8e366171fae5c2a
Barbara Hernandez <barbarahernandezrivero@MacBook-Air.local>
2021-04-07 22:17:26 -0500

    Changes to read the csv file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,25 @@
-#Open file
-file = '/Users/barbarahernandezrivero/Documents/GitHub/python-challenge/PyBank/Resources/budget_data.csv'
-with open(file,'r') as text:
-    print(text)
-    lines = text.read()
-    print(lines)
+#Import module to make data managable
+import os
+#Import module to read/manage csv files
+import csv
+
+csvpath = os.path.join('/Users','barbarahernandezrivero','Desktop','RepoGitLab','tdm-mxc-virt-data-pt-03-2021-u-c','Lessons','03-Python','Homework','Instructions','PyBank','Resources','budget_data.csv')
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+
+
     
